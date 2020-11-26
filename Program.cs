360cf85c35a7296fad48b7ba38d792cdc5ade946
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-11-26 08:41:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number less than 200 and above -200");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < -200 || num > 200)
+                {
+                    Console.WriteLine("Enter a number less than 200 and above -200");
+                    num = int.Parse(Console.ReadLine());
+                }
+
+                if (num <= 100)
+                {
+                    for (int i = num; i <= 100; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                else if (num >= 100)
+                {
+                    for (int i = num; i >= 100; i--)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
